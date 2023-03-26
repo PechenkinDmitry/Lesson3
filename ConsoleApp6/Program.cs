0942964cc5dd400c795bc636293c481df253289a
DmitryPechenkin <dimape4enb1998@gmail.com>
2023-03-26 19:55:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿public void Main()
+{ 
+class Doctor
+{
+    public string Name = "";
+    public int age = 0;
+    public int expirience = 0;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}  Age: {age}  Expirience {expirience}" );
+    }
+}
+class Surgeon : Doctor
+{
+    public new string Name = "";
+
+    public int Age = 0;
+        public void help()
+    {
+        Console.WriteLine($"Help from gemorroi");
+        }
+}
+}
